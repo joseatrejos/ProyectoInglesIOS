f25c33fbe82f6144a3814643fc6da7819cf3922c
Antonio <itonygame@gmail.com>
2019-08-27 20:19:22 -0700

    constraints

---
+++ ProyectoIngles/animalitosController.swift
@@ -0,0 +1,28 @@
+//
+//  animalitosController.swift
+//  ProyectoIngles
+//
+//  Created by equipo on 8/27/19.
+//  Copyright © 2019 Fer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class animalitosController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
